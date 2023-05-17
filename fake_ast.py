13253950560c4238de87c9d8cbf6431cf9f6b2cb
Jeong YunWon <jeong@youknowone.org>
2023-05-17 14:44:53 +0900

    Fix fakeast

--- fake_ast.py
+++ fake_ast.py
@@ -1,13 +1,13 @@
 import ast
 import _ast
 
-import rustpython_parser_pyo3
+import baembal
 
 orig = _ast.AST
         
 class ASTType(type):
     def __instancecheck__(self, instance):
-        return isinstance(instance, (orig, rustpython_parser_pyo3.unlocated_ast.AST))
+        return isinstance(instance, (orig, baembal.unlocated_ast.AST))
 
 class AST(ast.AST, metaclass=ASTType):
     pass
